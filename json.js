a3bb2b1b88e7d04491728c3128c9ded6bf831ca9
Dohu <adc0612@gmail.com>
2020-08-25 20:13:39 +0900

    200825 json study completed

--- json.js
+++ json.js
@@ -39,9 +39,19 @@ console.log(json);
 // 2. JSON to object
 // parse(json)
 json = JSON.stringify(rabbit);
-const obj = JSON.parse(json);
+const obj = JSON.parse(json, (key,value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
 console.log(obj);
 // rabbit에는 jump function이 있었지만 JSON으로 변환하면서 function이 들어오지 않았고
 // 그 결과 obj는 rabbit을 json으로 바꾸고 난 것을 다시 변환하면서 jump function은 없다.
 rabbit.jump();
 // obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+// rabbit에는 date가져오는 getDate function이 있었지만 JSON으로 변환하면서 function이 string으로 변환해서 담겼고
+// 그 결과 obj의 getDate는 json의 string으로 된 값이기 떄문에 getDate함수가 없다.
+// console.log(obj.birthDate.getDate());
+// 가져오려면 function을 재 설정해준다.
+console.log(obj.birthDate.getDate());
